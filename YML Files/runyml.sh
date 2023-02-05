afc1cdadc48a86daf2fb0c1e94b94d610b89a5ce
TMR <terrirol7@gmail.com>
2023-02-05 13:24:02 -0500

    fix

--- YML Files/runyml.sh
+++ YML Files/runyml.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd /home/ubuntu/setup/terrasetup/YML Files;
+cd /home/ubuntu/setup/terrasetup/YML Files || exit;
 kubectl apply -f myapp.yml;
 kubectl apply -f myrc.yml;
 kubectl apply -f pod.yml;
