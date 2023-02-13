d0dd29f683bd49273e015503ac68a93feea5afe4
TMR <terrirol7@gmail.com>
2023-02-13 14:27:17 -0500

    updateshell

--- kops/kops.sh
+++ kops/kops.sh
@@ -20,3 +20,7 @@
     sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
     sudo chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin/kubectl
+
+# create S3 bucket
+    aws s3 mb s3://tmrs3bucket
+    aws s3 ls # to verify
